11a0a208905dac90c79af14963d18c506f18900e
andyanday33 <andyanday33@gmail.com>
2023-03-30 22:43:11 +0100

    style: change navbar link styling

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,16 +1,43 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import styles from "@/styles/Navbar.module.scss";
 
 type Props = {
   className: string;
 };
 
 export const Navbar: React.FC<Props> = ({ className }) => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const routes = {
+    home: "/",
+    projects: "/projects",
+    posts: "/posts",
+    resume: "/x",
+  };
+
   return (
-    <header className={className}>
-      <Link href="/">Home</Link>
-      <Link href="/about">Projects</Link>
-      <Link href="/posts">Posts</Link>
+    <header className={styles.navbar}>
+      <Link
+        href={routes.home}
+        className={currentRoute === routes.home ? styles.active : ""}
+      >
+        Home
+      </Link>
+      <Link
+        href={routes.projects}
+        className={currentRoute === routes.projects ? styles.active : ""}
+      >
+        Projects
+      </Link>
+      <Link
+        href={routes.posts}
+        className={currentRoute === routes.posts ? styles.active : ""}
+      >
+        Posts
+      </Link>
       <Link href="/x">Resume</Link>
       <section>
         <a>T</a>
