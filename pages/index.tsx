0af550ee433d482a3024b88b5e9fb48d31165220
andyanday33 <andyanday33@gmail.com>
2023-03-31 19:46:22 +0200

    feat: add technology chips

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,23 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { AnimatedHero } from "@/components/AnimatedHero";
 import styles from "@/styles/Home.module.scss";
+import Link from "next/link";
+
+import {
+  SiAngular,
+  SiCsharp,
+  SiDotnet,
+  SiGraphql,
+  SiJavascript,
+  SiMicrosoftazure,
+  SiNextdotjs,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+} from "react-icons/si";
+
+import { DiSass } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +43,83 @@ export default function Home() {
             I am a <span className={styles.emphasize}>software engineer</span>,
             enthusiastic and passionate about web development technologies.
           </p>
+        </section>
+        <section className={styles.technologies}>
+          <p>I like to work and have experience with</p>
+          <section className={styles.techChipGroup}>
+            <div className={styles.techChip}>
+              <Link href="/tags/typescript">
+                <SiTypescript className={styles.icon} />
+              </Link>
+              <span>Typescript</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/javascript">
+                <SiJavascript className={styles.icon} />
+              </Link>
+              <span>Javascript</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/react">
+                <SiReact className={styles.icon} />
+              </Link>
+              <span>React</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/nextjs">
+                <SiNextdotjs className={styles.icon} />
+              </Link>
+              <span>Next.js</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/angular">
+                <SiAngular className={styles.icon} />
+              </Link>
+              <span>Angular</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/nodejs">
+                <FaNodeJs className={styles.icon} />
+              </Link>
+              <span>Node.js</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/csharp">
+                <SiCsharp className={styles.icon} />
+              </Link>
+              <span>C#</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/dotnet">
+                <SiDotnet className={styles.icon} />
+              </Link>
+              <span>DotNet</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/azure">
+                <SiMicrosoftazure className={styles.icon} />
+              </Link>
+              <span>Azure</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/sass">
+                <DiSass className={styles.icon} />
+              </Link>
+              <span>Sass</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/redux">
+                <SiRedux className={styles.icon} />
+              </Link>
+              <span>Redux</span>
+            </div>
+            <div className={styles.techChip}>
+              <Link href="/tags/graphql">
+                <SiGraphql className={styles.icon} />
+              </Link>
+              <span>GraphQL</span>
+            </div>
+          </section>
         </section>
       </section>
     </>
