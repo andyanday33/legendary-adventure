f103fb748770aa3fa6b12a600c12751014d5e50c
andyanday33 <andyanday33@gmail.com>
2023-04-01 18:30:24 +0200

    style: change animated hero responsiveness

--- components/AnimatedHero.tsx
+++ components/AnimatedHero.tsx
@@ -28,12 +28,18 @@ export const AnimatedHero = (props: Props) => {
         <span className={styles.accent}>{"}"}</span>
         <span className={styles.detail}>;</span>
       </h1>
+      <h1 className={styles.title + " " + styles.smallScreenOnly}>
+        rubber<span className={styles.primary}>.</span>
+        <span className={styles.accent}>duck{"("}</span>
+        <span className={styles.detail}>{"`"}</span>
+      </h1>
       <div className={styles.consoleLog}>
-        <h1 className={styles.title}>
+        <h1 className={styles.title + " " + styles.bigScreenOnly}>
           rubber<span className={styles.primary}>.</span>
           <span className={styles.accent}>duck{"("}</span>
           <span className={styles.detail}>{'"'}</span>
         </h1>
+
         <div className={styles.textContent}>
           <span className={styles.greeting + " " + styles.en}>
             Welcome to Berke&apos;s Domain
@@ -48,13 +54,27 @@ export const AnimatedHero = (props: Props) => {
             Bienvenido al dominio de berke
           </span>
         </div>
-
-        <h1 className={styles.closure + " " + styles.title}>
+        <h1
+          className={
+            styles.closure + " " + styles.title + " " + styles.bigScreenOnly
+          }
+        >
           <span className={styles.detail}>{'"'}</span>
+
           <span className={styles.accent}>{")"}</span>
           <span className={styles.detail}>;</span>
         </h1>
       </div>
+      <h1
+        className={
+          styles.closure + " " + styles.title + " " + styles.smallScreenOnly
+        }
+      >
+        <span className={styles.detail}>{"`"}</span>
+
+        <span className={styles.accent}>{")"}</span>
+        <span className={styles.detail}>;</span>
+      </h1>
     </>
   );
 };
