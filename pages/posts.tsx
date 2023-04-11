3418b5b1b88e147ae576e3b89f38985e19e0f14e
andyanday33 <andyanday33@gmail.com>
2023-04-11 22:13:01 +0200

    feat: add posts page draft

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import styles from "@/styles/Posts.module.scss";
 
 type Props = {};
 
 export default function Posts(props: Props) {
   return (
-    <>
+    <div className={styles.posts}>
       <h2>Posts</h2>
-    </>
+      <p>Coming soon...</p>
+    </div>
   );
 }
