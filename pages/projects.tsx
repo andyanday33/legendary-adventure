b226e976fb0daf86838bdc10c3a60887e6b86466
andyanday33 <andyanday33@gmail.com>
2023-04-11 22:06:13 +0200

    style: change to a simpler animation

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -33,7 +33,6 @@ const Projects: React.FC<ProjectProps> = ({ data }) => {
               initial={{ scale: 0.8, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.8, opacity: 0 }}
-              transition={{ type: "spring" }}
             >
               <Project
                 meta={{ title: project.title, tags: project.tags }}
